b9278e067531279a8062ed0fb0fd938d8bc1c85d
icheff <email@yoursite.com>
2024-03-25 13:43:00 +0100

    Add new file scripts/header.js

---
+++ scripts/header.js
@@ -0,0 +1,1 @@
+fetch('./header').then(response => response.ok ? response.text() : Promise.reject('Not found')).then(headerData => document.getElementById('header').innerHTML = headerData).catch(error => console.error(error.message));
