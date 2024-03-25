c5a6557813a7ec86529858776cb728726b6f71f2
icheff <email@yoursite.com>
2024-03-25 13:43:01 +0100

    Add new file scripts/footer.js

---
+++ scripts/footer.js
@@ -0,0 +1,1 @@
+fetch('./footer').then(response => response.ok ? response.text() : Promise.reject('Not found')).then(footerData => document.getElementById('footer').innerHTML = footerData).catch(error => console.error(error.message));
